42dca910a3198c6ae1c96152f754e398764fff4d
niudakok <niudakok@example.com>
2026-07-17 17:20:18 +0800

    fix: add default export for ES module compatibility

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -964,3 +964,4 @@ function updateStats(startTime, success) {
 function generateRequestId() {
     return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
 }
+export { onRequest as default };
